90b1f5659df4c0761fd523af26443b350e87ee83
antonioace <72002515+antonioace@users.noreply.github.com>
2022-04-20 20:54:01 -0500

    Update RegistroMantenimiento.jsx

--- src/components/mantenimiento/RegistroMantenimiento.jsx
+++ src/components/mantenimiento/RegistroMantenimiento.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 
 function RegistroMantenimiento() {
-  return (
-    <div>RegistroMantenimiento</div>
+ return (
+   <div>
+    <h1 className="text-center text-success">Registro Mantenimientos</h1>
+    <Form.Group className="py-2"  controlId="formBasicEmail">
+                <Form.Label>Placa del carro</Form.Label>
+                <Form.Control
+                        as="select"
+                        
+                      >
+                        <option>Escoja..</option>
+                        <option>Efectivo</option>
+                        <option>Debito</option>
+                        <option>Credito</option>
+                       
+                      </Form.Control>
+            </Form.Group>
+    <Form>
+    <Form.Group className="py-2"controlId="formBasicEmail">
+        <Form.Label>Kilometraje mantenimiento </Form.Label>
+        <Form.Control type="number" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group className="py-2" controlId="formBasicEmail">
+        <Form.Label>costo mantenimiento </Form.Label>
+        <Form.Control type="number" placeholder="Enter email" />
+    </Form.Group>
+
+   
+
+    <Form.Group className="py-2"  controlId="formBasicEmail">
+                <Form.Label>Forma de  pago </Form.Label>
+                <Form.Control
+                        as="select"
+                        
+                      >
+                        <option>Escoja..</option>
+                        <option>Efectivo</option>
+                        <option>Debito</option>
+                        <option>Credito</option>
+                       
+                      </Form.Control>
+            </Form.Group>
+<Button type="submit" variant="success" className="mt-3">
+  Registrar
+</Button>
+
+</Form>
+</div>
   )
 }
 
